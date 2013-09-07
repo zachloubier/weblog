bc4b5ce0012979b5ff295a18e7fe90fe028ead5c
Zach Loubier <zach@muvdigital.com>
2013-09-07 14:51:04 -0400

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Weblog::Application.config.secret_token = '8199f2a2a3b66673282d33ef9d70c4faa908e9e8ea5f54b8e92aee4a61deb90cc96dc3c6eba4e7ac75322035609f8c66de8ef676aa3c547831dca75948abff10'
